ef18681e7fd7f9aca95a6c0ac0d088d49846bd8c
manux21 <manux21@yandex.ru>
2026-02-16 22:15:03 +0300

    install husky

---
+++ frontend/eslint.config.js
@@ -0,0 +1,27 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+      globals: {
+        defineProps: 'readonly',
+        defineEmits: 'readonly',
+        defineExpose: 'readonly',
+        withDefaults: 'readonly',
+      },
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  }
+)
